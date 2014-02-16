2c2fdaaad57debb23939dee62f50dfe7ac7c3518
David Drabik <david@ddrabik.com>
2014-02-16 23:00:27 +0000

    added bash aliases file to dotfiles

---
+++ bash_aliases
@@ -0,0 +1,3 @@
+# Python virtual environments
+alias venvon='. venv/bin/activate'
+alias venvoff='deactivate'
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,9 +8,12 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc"    		  # list of files/folders to symlink in homedir
 
-##########
+# list of files/folders to symlink in homedir
+files="vimrc
+bash_aliases"
+
+#########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
